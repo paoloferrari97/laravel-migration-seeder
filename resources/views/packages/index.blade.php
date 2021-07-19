d2092fc2aad744aeda73ff71541882510ec765d1
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-07-19 18:14:53 +0200

    Creo e stampo la tabella in packages.index

--- resources/views/packages/index.blade.php
+++ resources/views/packages/index.blade.php
@@ -3,5 +3,39 @@
 @section('title', 'Pacchetti vacanza')
 
 @section('main')
-    vedi fumetti
+    <h1 class="text-center">Pacchetti viaggi</h1>
+
+    <div class="container">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Destinazione</th>
+                    <th scope="col">Prezzo</th>
+                    <th scope="col">Giorni di permanenza</th>
+                    <th scope="col">Data di partenza</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($pacchetti as $package)
+                    <tr>
+                        <th scope="row">{{ $package->id }}</th>
+                        <td>{{ $package->destinazione }}</td>
+                        <td>{{ $package->prezzo }}</td>
+                        <td>{{ $package->giorni_permanenza }}</td>
+                        <td>{{ $package->data_partenza }}</td>
+                        <td>
+                            {{-- <a href="{{ route('packages.show', $package->id) }}">View</a> | <a
+                                href="{{ route('packages.edit', $package->id) }}">Edit</a>
+                            <form action="{{ route('packages.destroy', $package->id) }}" method='post'>
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" @click="cancel($event)" class="btn btn-danger">!DELETE!</button>
+                            </form> --}}
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
