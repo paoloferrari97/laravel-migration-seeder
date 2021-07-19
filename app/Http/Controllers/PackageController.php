eb04345351f4ed9c057f8f3912a93ee4020ac030
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-07-19 18:26:42 +0200

    Creo la pagina show.blade.php

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -48,7 +48,7 @@ class PackageController extends Controller
      */
     public function show(Package $package)
     {
-        //
+        return view('packages.show', compact('package'));
     }
 
     /**
---
+++ resources/views/packages/show.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.app')
+
+@section('title', 'Pacchetto Vacanza')
+
+@section('main')
+    <ol>
+        <li>Id: {{ $package->id }}</li>
+        <li>Destinazione: {{ $package->destinazione }}</li>
+        <li>Prezzo: {{ $package->prezzo }}</li>
+        <li>Giorni di permanenza: {{ $package->giorni_permanenza }}</li>
+        <li>Data di partenza: {{ $package->data_partenza }}</li>
+
+        {{-- <form action="{{ route('packages.destroy', $package->id) }}" method='post'>
+        @csrf
+        @method('DELETE')
+        <button type="submit" @click="cancel($event)" class="btn btn-danger">!DELETE!</button>
+    </form> --}}
+
+    </ol>
+@endsection
